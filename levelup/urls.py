ea71d9508818213941519fd9064c4df6224cb81b
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-05-20 20:35:47 -0500

    added retrieve and list to game_types, games, and events

--- levelup/urls.py
+++ levelup/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
+from rest_framework import routers
+from levelupapi.views import GameTypeView, GameView, EventView
 from django.contrib import admin
 from django.urls import path
 
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'games', GameView, 'game')
+router.register(r'events', EventView, 'event')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
---
+++ levelupapi/views/__init__.py
@@ -0,0 +1,3 @@
+from .game_type import GameTypeView
+from .game_views import GameView
+from .event_views import EventView
