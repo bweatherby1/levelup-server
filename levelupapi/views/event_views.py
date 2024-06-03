8fbab7db31c700c89a07190715ce4e9b98f16b87
Brad Weatherby <bweatherby1@gmail.com git config --global user.name Brad>
2024-06-03 00:29:22 -0500

    added signup and delete

--- levelupapi/views/event_views.py
+++ levelupapi/views/event_views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import serializers, status
-from levelupapi.models import Event
+from levelupapi.models import Event, EventGamer, Gamer
 
 
 class EventView(ViewSet):
@@ -58,6 +59,29 @@ class EventView(ViewSet):
         event = Event.objects.get(pk=pk)
         event.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    @action(methods=['post'], detail=True)
+    def signup(self, request, pk):
+        """Post request for a user to sign up for an event"""
+
+        gamer = Gamer.objects.get(uid=request.data["user_id"])
+        event = Event.objects.get(pk=pk)
+        attendee = EventGamer.objects.create(
+            gamer=gamer,
+            event=event
+        )
+        return Response({'message': 'Gamer added'}, status=status.HTTP_201_CREATED)
+    
+    @action(methods=['delete'], detail=True)
+    def leave(self, request, pk):
+        """Delete request for a user to leave an event"""
+        
+        gamer=Gamer.objects.get(uid=request.data["user_id"])
+        event = Event.objects.get(pk=pk)
+        event_gamer = EventGamer.objects.get( gamer=gamer, event=event)
+        event_gamer.delete()
+        return Response({'message': 'Gamer removed'}, status=status.HTTP_204_NO_CONTENT)
+
         
 
 class EventSerializer(serializers.ModelSerializer):
